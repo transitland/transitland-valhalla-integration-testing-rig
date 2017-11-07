e7e318b198c007a4b8964de50b13391196dfa001
Ian Rees <ian@ianrees.net>
2017-11-06 18:16:04 -0800

    wget fix

--- scripts/build_and_route.sh
+++ scripts/build_and_route.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-source `dirname "$0"`/tiles_env.sh
 
-echo "[INFO] Building transit tiles"
-scripts/build_transit_tiles.sh
-scripts/validate_transit_tiles.sh
+# build using Transitland API, validate, run valhalla_route_service
+
+cwd="$(cd "$(dirname "$0")"; pwd)"
+source $cwd/tiles_env.sh
+
+echo "[INFO] Building transit tiles: ${1}"
+$cwd/build_transit_tiles.sh $1
+$cwd/validate_transit_tiles.sh
 
 echo "[INFO] Downloading extract"
-cwd=`pwd`
-mkdir -p $DATA_DIR/extracts && cd $DATA_DIR/extracts && wget https://s3.amazonaws.com/metro-extracts.mapzen.com/san-francisco-bay_california.osm.pbf && cd $cwd
+mkdir -p $DATA_DIR/extracts
+wget https://s3.amazonaws.com/metro-extracts.mapzen.com/san-francisco-bay_california.osm.pbf -O $DATA_DIR/extracts/extract.osm.pbf
 
 echo "[INFO] Building Valhalla routing tiles"
-valhalla_build_tiles --conf $CONF_DIR/valhalla.json $DATA_DIR/extracts/san-francisco-bay_california.osm.pbf
+valhalla_build_tiles --conf $CONF_DIR/valhalla.json $DATA_DIR/extracts/extract.osm.pbf
 
 echo "[INFO] Starting valhalla_route_service"
 valhalla_route_service $CONF_DIR/valhalla.json
